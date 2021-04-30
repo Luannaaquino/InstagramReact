8f9df1c343b34cf2c328b3b86ae1ab877e8b4800
Luanna Aquino <luannaluleticia@gmail.com>
2021-04-30 17:55:22 -0300

    componentização inicial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./components/navbar";
+import Body from "./components/body";
+import BottomMobile from "./components/bottomMobile "
+
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Body />
+      <BottomMobile />
+    </div>
+  );
+}
